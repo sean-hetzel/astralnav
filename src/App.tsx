e0a34dd86105f50d8fb5e63d00dc94ad30c2c90a
Seanhetzel <seanhetzel1@gmail.com>
2020-04-11 23:18:16 -0700

    added icons and playin around with Neumorphism

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <div className="btn-normal "><i className="tim-icons tim-icons-lg icon-sound-wave teal"/></div>
+      <div className="btn-normal"><i className="tim-icons tim-icons-lg icon-world teal"/></div>
+      <div className="btn-normal"><i className="tim-icons tim-icons-lg icon-spaceship teal"/></div>
+
+      <div className="btn-special"></div>
+      {/* <div className="glow">
+        <h1 className="teal">hi</h1>
+      </div>
+      <h1>hi</h1>
+      <h1 className="yellow">Yellow</h1>
+      <h1 className="teal">Teal</h1>
+      <div className="grad">
+        <h1 className="darker">hi</h1>
+      </div> */}
+    </>
+  );
 }
 
 export default App;
