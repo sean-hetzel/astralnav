0d646d7bfa83377c6b3970ccafc55bc17c909b2b
Seanhetzel <seanhetzel1@gmail.com>
2020-04-15 23:10:59 -0700

    added logo and brand text to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import logo from "../images/astral-nav-logo-300x300.png";
 
 interface Props {}
 interface State {}
@@ -9,23 +10,26 @@ export default class NavBar extends Component<Props, State> {
 
   render() {
     return (
-      <nav >
-        <a href="/" className="brand-img"><img src="" alt="Atral Nav"/></a>
+      <nav>
+        <a href="/" className="brand-link">
+          <img src={logo} alt="Atral Nav" className="brand-img" />
+          <h1 className="brand-text">ASTRAL NAV</h1>
+        </a>
         <Link className="btn nav-btn" to="/">
-          <i className="tim-icons tim-icons-lg icon-world teal glow" />
-          <h2 className="nav-text">Home</h2>
+          <i className="tim-icons tim-icons-lg icon-world teal" />
+          <h4 className="nav-text">Home</h4>
         </Link>
         <Link className="btn nav-btn" to="/">
-          <i className="tim-icons tim-icons-lg icon-vector teal glow " />
-          <h2 className="nav-text">Progress</h2>
+          <i className="tim-icons tim-icons-lg icon-vector teal " />
+          <h4 className="nav-text">Progress</h4>
+        </Link>
+        <Link className="btn nav-btn" to="/">
+          <i className="tim-icons tim-icons-lg icon-support-17 teal" />
+          <h4 className="nav-text">Support</h4>
         </Link>
         <Link className="btn nav-btn login" to="/">
-          <i className="tim-icons tim-icons-lg icon-support-17 teal glow" />
-          <h2 className="nav-text">Support</h2>
-        </Link>
-        <Link className="btn nav-btn login" to="/">
-          <i className="tim-icons tim-icons-lg icon-single-02 teal glow" />
-          <h2 className="nav-text">Login</h2>
+          <i className="tim-icons tim-icons-lg icon-single-02 teal" />
+          <h4 className="nav-text">Login</h4>
         </Link>
       </nav>
     );
