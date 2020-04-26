39b8abe7717b58b5f08f818954790b1ca4789842
Seanhetzel <seanhetzel1@gmail.com>
2020-04-25 22:49:31 -0700

    working on grid

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,27 +12,31 @@ export default class Home extends Component<Props, State> {
     return (
       <>
         <NavBar />
-        <div className="system">
-          <div className="star young small"></div>
-          <div className="planet large">
-            <div className="moon"></div>
-          </div>
-          <div className="planet">
+        <div className="grid">
+        <div></div>
+          <div className="system">
+            <div className="star young small"></div>
+            <div className="planet large">
+              <div className="moon"></div>
+            </div>
+            <div className="planet">
+              <div className="belt"></div>
+            </div>
+            <div className="asteroids"></div>
+            <div className="planet"></div>
+            <div className="belt"></div>
+            <div className="planet small"></div>
+            <div className="planet small"></div>
             <div className="belt"></div>
           </div>
-          <div className="asteroids"></div>
-          <div className="planet"></div>
-          <div className="belt"></div>
-          <div className="planet small"></div>
-          <div className="planet small"></div>
-          <div className="belt"></div>
-        </div>
-        <div id="sign-up-container">
-          <h1>Navigate Your Job Search</h1>
-          <p>bla bla bla</p>
-          <Link className="btn-main btn" id="login" to="/signup">
-            <span>Sign Up</span>
-          </Link>
+          <div></div>
+          <div id="sign-up-container">
+            <h1>Navigate Your Job Search</h1>
+            <p>Track your job applications, networking evernts and interviews all in one place. </p>
+            <Link className="btn-main btn" to="/signup">
+              <span>Sign Up</span>
+            </Link>
+          </div>
         </div>
       </>
     );
