0b9945b040809b766556885ac60a630a1d8e47b4
Seanhetzel <seanhetzel1@gmail.com>
2020-04-25 21:12:40 -0700

    adding orrery

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,11 +11,24 @@ export default class Home extends Component<Props, State> {
     return (
       <>
         <NavBar />
-        <div id="astral-img"></div>
+        <div className="system">
+          <div className="star young small"></div>
+          <div className="planet large">
+            <div className="moon"></div>
+          </div>
+          <div className="planet">
+            <div className="belt"></div>
+          </div>
+          <div className="asteroids"></div>
+          <div className="planet"></div>
+          <div className="belt"></div>
+          <div className="planet small"></div>
+          <div className="planet small"></div>
+          <div className="belt"></div>
+        </div>
         <div id="sign-up-container">
           <h1>Navigate your Job Search</h1>
           <p>bla bla bla</p>
-          
         </div>
       </>
     );
