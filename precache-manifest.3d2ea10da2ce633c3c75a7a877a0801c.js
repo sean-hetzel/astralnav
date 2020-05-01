f883a8cac3305258d7fe795dfa223b480d4bbe83
Seanhetzel <seanhetzel1@gmail.com>
2020-04-30 18:57:45 -0700

    Updates

---
+++ precache-manifest.3d2ea10da2ce633c3c75a7a877a0801c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c988746ed2a5924605e21933da4218a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad166c65b06840f1c95d",
+    "url": "/static/css/main.42b29977.chunk.css"
+  },
+  {
+    "revision": "8b1e1e849023dc5cc7a5",
+    "url": "/static/js/2.13284cab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.13284cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad166c65b06840f1c95d",
+    "url": "/static/js/main.a1fa74e4.chunk.js"
+  },
+  {
+    "revision": "18c09646c0ceb3c75413",
+    "url": "/static/js/runtime-main.5066c173.js"
+  },
+  {
+    "revision": "317c83ea61eab172f8a1dad69e286ebe",
+    "url": "/static/media/Ailerons-Regular.317c83ea.otf"
+  },
+  {
+    "revision": "2d644394559e2060dcd321bee90bdce4",
+    "url": "/static/media/astral-nav-dawn.2d644394.svg"
+  },
+  {
+    "revision": "8b6bf6e08dc18887edfd7e1f7feb5cd8",
+    "url": "/static/media/astral-nav-logo-300x300.8b6bf6e0.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/astralnav/precache-manifest.ab2bcd54edcc1ed1f6f21f5dbe016ed7.js"
+  "/precache-manifest.3d2ea10da2ce633c3c75a7a877a0801c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/astralnav/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
