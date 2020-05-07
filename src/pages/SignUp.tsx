a7cf56c3dd1aadb4a3176a19c626c2d3744149eb
Seanhetzel <seanhetzel1@gmail.com>
2020-05-06 17:53:45 -0700

    added palm images to sign up

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import SignUpForm from "../components/SignUpForm";
 import LiftOffImg from "../images/astral-nav-dawn-2.svg";
+import PalmBottomLeft from "../images/palm-bottom-left.svg";
+import PalmBottomRight from "../images/palm-bottom-right.svg";
+import PalmTopRight from "../images/palm-top-right.svg";
 
 interface Props
   extends React.DetailedHTMLProps<
@@ -19,7 +22,29 @@ export default class SignUp extends Component<Props, State> {
         <div id="form-container">
           <SignUpForm />
         </div>
-        <img id="lift-off-img" src={LiftOffImg} alt="rocket lifting off at dawn"/>
+        <img
+          id="lift-off-img"
+          src={LiftOffImg}
+          alt="rocket lifting off at dawn"
+        />
+        <img
+          className="palm"
+          id="palm-bottom-left"
+          src={PalmBottomLeft}
+          alt="palm bottom left"
+        />
+        <img
+          className="palm"
+          id="palm-bottom-right"
+          src={PalmBottomRight}
+          alt="palm bottom right"
+        />
+        <img
+          className="palm"
+          id="palm-top-right"
+          src={PalmTopRight}
+          alt="palm top right"
+        />
       </>
     );
   }
